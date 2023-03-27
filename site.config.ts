9ef7dfd20e5aa831c3c4507e648417f066a838e8
ZhuTim <129023204+ZhuTim@users.noreply.github.com>
2023-03-27 15:23:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '06e119bec18c4ffbb94013c2598e9347',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Wandz Blog',
+  domain: 'https://jade-denim-b55.notion.site/d191b5a5002a44e0a3ae4cf272eb75de?v=06e119bec18c4ffbb94013c2598e9347',
+  author: 'Zhu Tim',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'ZhuTim',
+  github: 'ZhuTim',
+  linkedin: 'ZhuTim',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
